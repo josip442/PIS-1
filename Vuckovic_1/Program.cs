0b2dce7d7fc1fa9587be07a300447e191fec60d1
DinoZzo99 <66387612+DinoZzo99@users.noreply.github.com>
2021-03-23 20:32:09 +0100

    Update Program.cs

--- Vuckovic_1/Program.cs
+++ Vuckovic_1/Program.cs
@@ -8,6 +8,8 @@ namespace Vuckovic_1
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Novo");
+            
+            Console.WriteLine("Novije");
         }
     }
 }
